b65d4017b99fb0554724f64e821678ab02d9371b
unknown <kanbarkarakash45@gmail.com>
2021-04-12 22:29:36 -0700

    added navigation using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import HomePage from './pages/homepage/homepage.component'
+import {Route,Switch} from 'react-router-dom'
 import './App.css'
+
+const hatsPage = ()=> (
+  <div>
+    Hats Page
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={hatsPage}/>
+      </Switch>
+     
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 import './menu-item.scss'
 
-const MenuItem = ({title,imageUrl,size}) => (
-    <div  className={`${size} menu-item`}>
+const MenuItem = ({title,imageUrl,size,linkUrl,history,match}) => (
+    <div  className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}/>
         <div className='content'>
-            <hi className='title'>{title.toUpperCase() }</hi>
-            <span classNam='subtitle'>Shop Now</span>
+            <h1 className='title'>{title.toUpperCase() }</h1>
+            <span className='subtitle'>Shop Now</span>
         </div>
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
